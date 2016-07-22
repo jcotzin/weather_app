4cb00685b0951b987f2576b7d50db5ef46367c06
Jessica Cotzin <jcotzin@gmail.com>
2016-07-22 14:40:44 -0400

    added API and functionality

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,4 +1,16 @@
+require 'httparty'
+
 class WeatherController < ApplicationController
   def index
+    @today = Date.today
+
+    @response = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?q=miami&APPID=612104db0b85a84d80dd7cd48d5516c9')
+    @parsed = @response.parsed_response
+    @description = @response.parsed_response["weather"][0]["description"]
+    @temp = @parsed["main"]["temp"]
+    @image = @parsed["weather"][0]["icon"]
+    @fahrenheit = @temp * 1.8
+    @degrees = (@fahrenheit - 459.67).round
   end
+
 end
